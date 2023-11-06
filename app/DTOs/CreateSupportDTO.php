9e6074034daccfdb00a3ad47377b46d00c69e0fa
Julio <phalkao@gmail.com>
2023-11-06 11:35:06 -0300

    refactor: use dto pattern

---
+++ app/DTOs/CreateSupportDTO.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\DTOs;
+
+use App\Http\Requests\StoreUpdateSupportRequest;
+
+class CreateSupportDTO
+{
+    public function __construct(
+        public string $subject,
+        public string $status,
+        public string $body,
+    ) {}
+
+    public static function makeFromRequest(StoreUpdateSupportRequest $request): self
+    {
+        return new self(
+            $request->subject,
+            'A', // $request->status,
+            $request->body
+        );
+    }
+}
