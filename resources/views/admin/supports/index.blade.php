524eb1eef8e3aa9295ca01a3ad17a04e0977c79f
Julio <phalkao@gmail.com>
2023-11-07 04:19:49 -0300

    refactor: list all supports

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -15,13 +15,13 @@
     <tbody>
         @foreach ($supports as $support)
             <tr>
-                <td>{{ $support->subject }}</td>
-                <td>{{ $support->status}}</td>
-                <td>{{ $support->body}}</td>
+                <td>{{ $support['subject'] }}</td>
+                <td>{{ $support['status']}}</td>
+                <td>{{ $support['body']}}</td>
                 <td>
-                    <a class="" href="{{ route('supports.show', $support->id) }}">Exibir</a>
-                    <a class="" href="{{ route('supports.edit', $support->id) }}">Editar</a>
-                    <form action="{{ route('supports.destroy', $support->id) }}" method="post">
+                    <a class="" href="{{ route('supports.show', $support['id']) }}">Exibir</a>
+                    <a class="" href="{{ route('supports.edit', $support['id']) }}">Editar</a>
+                    <form action="{{ route('supports.destroy', $support['id']) }}" method="post">
                         @csrf() 
                         @method('DELETE')
                         <button type="submit">Deletar</button>
