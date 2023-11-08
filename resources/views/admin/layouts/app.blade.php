f6c69b64c24cde0bfe941b9c4961d5fa4e60a37c
Julio <phalkao@gmail.com>
2023-11-08 06:35:04 -0300

    feat: use tailwind

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title') - {{ config('app.name') }}</title>
+
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
     <head>
