15ac33957212d17c71ef1226db3834601dc79744
Julio <phalkao@gmail.com>
2023-11-08 05:52:27 -0300

    feat: list items api supports

--- app/Http/Controllers/Api/SupportController.php
+++ app/Http/Controllers/Api/SupportController.php
@@ -23,9 +23,25 @@ class SupportController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $supports = $this->service->paginate(
+            page: $request->get("page",1),
+            totalPerPage: $request->get("per_page",1),
+            filter: $request->filter
+        );
+
+        return SupportResource::collection($supports->items())
+                                ->additional([
+                                    'meta'=> [
+                                        'total'         => $supports->total(),
+                                        'is_first_page' => $supports->isFirstPage(),
+                                        'is_last_page'  => $supports->isLastPage(),
+                                        'current_page'  => $supports->currentPage(),
+                                        'next_page'     => $supports->getNumberNextPage(),
+                                        'previous_page' => $supports->getNumberPreviousPage()
+                                    ]
+                                ]);
     }
 
     /**
