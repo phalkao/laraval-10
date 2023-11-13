6f361f036dd6df6232532ca5de9a85c700454831
Julio <phalkao@gmail.com>
2023-11-13 17:19:17 -0300

    feat: configure templates tailwindcss

--- resources/views/auth/layouts/base.blade.php
+++ resources/views/auth/layouts/base.blade.php
@@ -5,10 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
     <title>@yield('title') - {{ config('app.name') }}</title>
 
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body>
